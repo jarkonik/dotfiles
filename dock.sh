a45b7389112ce14cf84f5c0b2d4ddfe348e5123c
Jaroslaw Konik <konikjar@gmail.com>
2019-01-14 23:17:22 +0100

    fix

--- dock.sh
+++ dock.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build $(dirname "$0") -t dotfiles &&
-winpty docker run -v /$(PWD):/mnt --rm --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -it dotfiles
+winpty docker run -v /$(PWD):/mnt -p 8000:8000 --rm --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -it dotfiles
