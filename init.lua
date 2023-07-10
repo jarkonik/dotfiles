a4d75290fb60a7b9192fb09b7882fe1fa94a4e5e
Jaroslaw Konik <konikjar@gmail.com>
2023-07-10 13:25:55 +0200

    improve

--- init.lua
+++ init.lua
@@ -66,6 +66,16 @@ require("lazy").setup({
 	"nvim-lualine/lualine.nvim",
 	"ojroques/nvim-osc52",
 	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
 })
 
 require("stickybuf").setup()
