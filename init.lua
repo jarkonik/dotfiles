66ac36f945b45883d694b331d119ce7134a5576c
Jaroslaw Konik <konikjar@gmail.com>
2023-07-14 21:46:29 +0200

    update

--- init.lua
+++ init.lua
@@ -344,24 +344,18 @@ require("scrollbar").setup()
 -- Status line
 require("lualine").setup()
 
--- OSC52 copy
+-- OSC52 copy/paste
 local function copy(lines, _)
 	require("osc52").copy(table.concat(lines, "\n"))
 end
-
 local function paste()
 	return { vim.fn.split(vim.fn.getreg(""), "\n"), vim.fn.getregtype("") }
 end
-
 vim.g.clipboard = {
 	name = "osc52",
 	copy = { ["+"] = copy, ["*"] = copy },
 	paste = { ["+"] = paste, ["*"] = paste },
 }
-
--- Now the '+' register will copy to system clipboard using OSC52
-vim.keymap.set("n", "<leader>c", '"+y')
-vim.keymap.set("n", "<leader>cc", '"+yy')
 
 -- Language Syntax
 require("nvim-treesitter.configs").setup({
