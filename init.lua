a5fa578241304b5ddfcc221cb46e0447bced521f
Jaroslaw Konik <konikjar@gmail.com>
2024-05-05 21:40:13 +0200

    update

--- init.lua
+++ init.lua
@@ -193,6 +193,22 @@ vim.keymap.set('n', '<leader>db', require("dap").toggle_breakpoint, {})
 vim.keymap.set('n', '<leader>dr', function()
 	vim.cmd.RustLsp('debug')
 end, {})
+vim.fn.sign_define('DapBreakpoint', {
+	text = '',
+	texthl = 'DapBreakpoint',
+	linehl = 'DapBreakpoint',
+	numhl =
+	'DapBreakpoint'
+})
+vim.fn.sign_define('DapBreakpointCondition',
+	{ text = 'ﳁ', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint', numhl = 'DapBreakpoint' })
+vim.fn.sign_define('DapBreakpointRejected',
+	{ text = '', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint', numhl = 'DapBreakpoint' })
+vim.fn.sign_define('DapLogPoint', { text = '', texthl = 'DapLogPoint', linehl = 'DapLogPoint', numhl = 'DapLogPoint' })
+vim.fn.sign_define('DapStopped', { text = '', texthl = 'DapStopped', linehl = 'DapStopped', numhl = 'DapStopped' })
+vim.api.nvim_set_hl(0, 'DapBreakpoint', { ctermbg = 0, fg = '#993939', bg = '#31353f' })
+vim.api.nvim_set_hl(0, 'DapLogPoint', { ctermbg = 0, fg = '#61afef', bg = '#31353f' })
+vim.api.nvim_set_hl(0, 'DapStopped', { ctermbg = 0, fg = '#98c379', bg = '#31353f' })
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
