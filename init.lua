a75939bc8dcffe6c4412d8afb899e6015dcc607d
Jaroslaw Konik <konikjar@gmail.com>
2024-06-22 11:08:41 +0200

    update

--- init.lua
+++ init.lua
@@ -303,8 +303,30 @@ cmp.setup({
 })
 
 require('lspconfig').basedpyright.setup {}
-require('lspconfig').lua_ls.setup({})
-require("lspconfig").pyright.setup({})
+require 'lspconfig'.lua_ls.setup {
+	on_init = function(client)
+		local path = client.workspace_folders[1].name
+		if vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc') then
+			return
+		end
+
+		client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+			runtime = {
+				version = 'LuaJIT'
+			},
+			workspace = {
+				checkThirdParty = false,
+				library = {
+					vim.env.VIMRUNTIME
+				}
+			}
+		})
+	end,
+	settings = {
+		Lua = {}
+	}
+}
+-- require("lspconfig").pyright.setup({})
 vim.api.nvim_create_autocmd('BufWritePre', {
 	callback = function()
 		vim.lsp.buf.format()
