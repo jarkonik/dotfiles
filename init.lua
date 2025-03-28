6055b061140328eee415a23a1367e1f69028335c
Jaroslaw Konik <konikjar@gmail.com>
2025-03-28 20:11:46 +0100

    update

--- init.lua
+++ init.lua
@@ -126,6 +126,21 @@ require("lazy").setup({
 		dependencies = { 'nvim-lua/plenary.nvim' }
 	},
 	{
+		'romgrk/barbar.nvim',
+		dependencies = {
+			'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+			'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+		},
+		init = function() vim.g.barbar_auto_setup = false end,
+		opts = {
+			-- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+			-- animation = true,
+			-- insert_at_start = true,
+			-- …etc.
+		},
+		version = '^1.0.0', -- optional: only update when a new 1.x version is released
+	},
+	{
 		"folke/zen-mode.nvim",
 		opts = {
 			window = {
