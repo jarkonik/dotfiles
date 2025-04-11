9154307f9e2be28c16dbfd777d4352ad7732e7c2
Jaroslaw Konik <konikjar@gmail.com>
2025-04-11 18:37:39 +0200

    u

--- init.lua
+++ init.lua
@@ -603,7 +603,7 @@ local telescope = require('telescope')
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<C-A-p>', builtin.commands, {})
-vim.keymap.set('n', '<leader><leader>', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>gh', telescope.extensions.git_file_history.git_file_history, {})
 
