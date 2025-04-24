577afaf7ac41fb15e02b2d71f37d80c9605ee79d
Jaroslaw Konik <konikjar@gmail.com>
2025-04-24 11:30:25 +0200

    update

--- wezterm.lua
+++ wezterm.lua
@@ -4,5 +4,6 @@ local config = wezterm.config_builder()
 config.default_prog = { '/usr/bin/tmux' }
 config.color_scheme = 'Kanagawa (Gogh)'
 config.enable_tab_bar = false
+config.skip_close_confirmation_for_processes_named = {}
 
 return config
